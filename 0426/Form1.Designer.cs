92ce8990b980930fd19403120a4c30d169e4302e
ozeki <hhdfgg@users.noreply.github.com>
2019-04-26 15:42:14 +0900

    Initial commit

---
+++ 0426/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace _0426
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aisatu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // aisatu
+            // 
+            this.aisatu.Location = new System.Drawing.Point(103, 114);
+            this.aisatu.Name = "aisatu";
+            this.aisatu.Size = new System.Drawing.Size(75, 23);
+            this.aisatu.TabIndex = 0;
+            this.aisatu.Text = "あいさつ";
+            this.aisatu.UseVisualStyleBackColor = true;
+            this.aisatu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.aisatu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button aisatu;
+    }
+}
+
---
+++ 0426/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0426
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            int a = 0;
+            a = 5 / a;
+            */
+
+            aisatu.Text = "こんにちは";
+            aisatu.Left = 0;
+            aisatu.Visible = false;
+
+
+            //あいさつ
+            MessageBox. Show("こんにちは");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
